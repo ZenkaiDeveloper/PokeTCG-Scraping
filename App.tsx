cc3199acead430519fb54b1720ea9ad92feddd92
ZenkaiDeveloper <wang.andy32@gmail.com>
2022-11-12 14:53:41 -0500

    1 file modified, 1 added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import './style.css';
+import PriceChartData from './Components/PriceChartData.jsx';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <PriceChartData />
     </div>
   );
 }
---
+++ Components/PriceChartData.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const PriceChartData = () => {
+  return (
+    <div>
+      <h1>Test</h1>
+    </div>
+  );
+};
+
+export default PriceChartData;
